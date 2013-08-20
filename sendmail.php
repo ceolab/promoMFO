a1dbbadbe0d402d6de5bf44f23561a19fe0719ab
EvGenius <evgenius@evgenius-M51Vr.(none)>
2013-08-21 01:50:52 +0400

    first commit

---
+++ sendmail.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: evgenius
+ * Date: 8/21/13
+ * Time: 12:04 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$text = "
+ Имя: %name%<br/>
+ Email: %email%
+
+";
